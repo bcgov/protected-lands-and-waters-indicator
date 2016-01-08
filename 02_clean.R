a9fda63bf5d877edd14209c34a09c804b4e5f8df
Andy Teucher <andy.teucher@gov.bc.ca>
2016-01-08 10:44:55 -0800

    Only do gBuffer if there are invalid polygons

--- 02_clean.R
+++ 02_clean.R
@@ -15,6 +15,7 @@ library(rgdal)
 library(sp)
 library(rgeos)
 library(raster)
+library(bcmaps)
 
 ## The CARTS database is downloadable from the Canadian Council on
 ## Ecological Areas here: http://www.ccea.org/carts/
@@ -25,19 +26,20 @@ bc_carts_orig <- carts[carts$LOC_E == "British Columbia", ]
 rm(carts)
 
 ## Transform CRS of bc_carts to BC Albers
-bc_carts_orig <- spTransform(bc_carts_orig, CRS(proj4string(ecoregions)))
+bc_carts <- spTransform(bc_carts_orig, CRS(proj4string(ecoregions)))
 
 #### Simplify for testing
 ## devtools::install_github("ateucher/rmapshaper")
-library(rmapshaper)
-bc_carts <- ms_simplify(bc_carts_orig, 0.01, keep_shapes = TRUE)
-ecoregions <- ms_simplify(ecoregions, 0.01, keep_shapes = TRUE)
+# library(rmapshaper)
+# bc_carts <- ms_simplify(bc_carts, 0.01, keep_shapes = TRUE)
+# ecoregions <- ms_simplify(ecoregions, 0.01, keep_shapes = TRUE)
 ####
 
 ## Check validity of polygons of bc_carts, fix with gBuffer trick, and check again:
-gIsValid(bc_carts, byid = TRUE)
-bc_carts <- gBuffer(bc_carts, byid = TRUE, width = 0)
-any(!gIsValid(bc_carts, byid = TRUE))
+if (any(!gIsValid(bc_carts, byid = TRUE))) {
+  bc_carts <- gBuffer(bc_carts, byid = TRUE, width = 0)
+  any(!gIsValid(bc_carts, byid = TRUE))
+}
 
 # Get separate layers for terrestrial and marine protected areas
 bc_carts_t <- bc_carts[bc_carts$BIOME == "T", ]
@@ -47,4 +49,4 @@ bc_carts_m <- bc_carts[bc_carts$BIOME == "M", ]
 bc_carts_t_unioned <- raster::union(bc_carts_t) # This is incredibly slow.
 bc_carts_m_unioned <- raster::union(bc_carts_m)
 
-save.image(file = "tmp/input_layers.rda")
+save.image(file = "tmp/input_layers_full.rda")
