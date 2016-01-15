a594dd6f4c94a883f6c4b37f939be007fdf71b8f
Andy Teucher <andy.teucher@gov.bc.ca>
2016-01-14 17:29:44 -0800

    Plotting work

--- 04_output.R
+++ 04_output.R
@@ -9,11 +9,11 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
-cum_summary$ecoregion <- tools::toTitleCase(cum_summary$ecoregion)
+cum_summary$ecoregion <- tools::toTitleCase(tolower(cum_summary$ecoregion))
 cum_summary <- order_df(cum_summary, "ecoregion", "cum_percent_protected", max, na.rm = TRUE, desc = TRUE)
 
-ggplot(cum_summary_plot, aes(x = prot_date, y = cum_percent_protected)) +
-  geom_path() + geom_point() +
+ggplot(cum_summary, aes(x = prot_date, y = cum_percent_protected)) +
+  geom_path() +
   facet_wrap(~ecoregion, labeller = label_wrap_gen())
 
 ## To much variation in size for this to be useful
