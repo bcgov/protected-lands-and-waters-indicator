4efab03211ba136ac160b6a4a466df32db4ad6e4
Andy Teucher <andy.teucher@gov.bc.ca>
2016-01-21 17:23:08 -0800

    Begin marine analysis

--- 03_analysis.R
+++ 03_analysis.R
@@ -88,12 +88,75 @@ cum_summary_t <- bind_rows(carts_eco_t_summary_by_year, carts_bc_t_summary_by_ye
          prot_date_full = paste0(prot_date, "-01-01")) %>%
   filter(!is.na(cum_area_protected))
 
-# Provincial summary ------------------------------------------------------
+# Marine analysis ----------------------------------------------------
+
+## Set the IUCN category as an ordered factor:
+bc_carts_m$IUCN_CAT = factor_iucn_cats(bc_carts_m$IUCN_CAT)
+
+## Get the earliest year of protection for polygon segments that overlap
+bc_carts_m_unioned$prot_date <- get_unioned_attribute(bc_carts_m_unioned, bc_carts_m, "PROTDATE", min, "numeric", na.rm = TRUE)
+
+## Get the minimum iucn category
+bc_carts_m_unioned$iucn <- get_unioned_attribute(bc_carts_m_unioned, bc_carts_m, "IUCN_CAT", min, "factor", na.rm = TRUE)
+
+## Get the row.names of the polygon with the minimum iucn category
+bc_carts_m_unioned$carts_id_min_iucn <- get_unioned_attribute(bc_carts_m_unioned, bc_carts_m, "IUCN_CAT", which_min, "integer")
+
+## Get areas of unioned polygons
+bc_carts_m_unioned$prot_area <- gArea(bc_carts_m_unioned, byid = TRUE)
+
+## Aggregate the protected areas by their protected date
+bc_carts_m_agg <- aggregate(bc_carts_m_unioned[, "prot_date"], by = c("prot_date"))
+
+###
+
+## Calculate the area of the ecoregions
+ecoregions_m$area <- rgeos::gArea(ecoregions_m, byid = TRUE)
+
+## Intersect ecoregions with protected areas
+carts_eco_m <- raster::intersect(ecoregions_m, bc_carts_m_agg)
+carts_eco_m <- rgeos::createSPComment(carts_eco_m) # Ensure polygon holes are properly identified
+
+## Calculate size of protected areas in each ecoregion
+carts_eco_m$prot_area <- rgeos::gArea(carts_eco_m, byid = TRUE)
+
+#bc_area_sq_m <- bc_area(units = "m2")
+
+## Summarize
+carts_eco_m_summary_by_year <- carts_eco_m@data %>%
+  filter(prot_date > 0) %>%
+  complete(c(CRGNNM, CRGNCD, area), prot_date, fill = list(prot_area = 0)) %>%
+  group_by(ecoregion = CRGNNM, ecoregion_code = CRGNCD, prot_date) %>%
+  summarise(ecoregion_area = min(area),
+            tot_protected = sum(prot_area),
+            percent_protected = tot_protected / ecoregion_area * 100)
+
+## Provincial summary
+carts_bc_m_summary_by_year <- bc_carts_m_unioned@data %>%
+  filter(prot_date > 0) %>%
+  group_by(prot_date) %>%
+  summarise(ecoregion = "British Columbia",
+            ecoregion_code = "BC",
+            ecoregion_area = sum(ecoregions_m$area),
+            tot_protected = sum(prot_area),
+            percent_protected = tot_protected / ecoregion_area * 100)
+
+cum_summary_m <- bind_rows(carts_eco_m_summary_by_year, carts_bc_m_summary_by_year) %>%
+  group_by(ecoregion, ecoregion_code) %>%
+  arrange(prot_date) %>%
+  mutate(cum_area_protected = cumsum(tot_protected),
+         cum_percent_protected = cumsum(percent_protected),
+         type = "All conservation lands",
+         prot_date_full = paste0(prot_date, "-01-01")) %>%
+  filter(!is.na(cum_area_protected))
+
+# Provincial summaries ------------------------------------------------------
 
 bc_area_ha <- bc_area(units = "ha")
 
 ## Get accurate areas:
 bc_carts$area_ha <- gArea(bc_carts, byid = TRUE) / 1e4
+
 bc_designation_summary <- bc_carts@data %>%
   group_by(BIOME, Designation = TYPE_E) %>%
   summarise(total_area_ha = sum(area_ha)) %>%
