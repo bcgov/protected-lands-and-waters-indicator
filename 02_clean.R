21185839ada6d310c88e5115317c711b2b3c9787
Andy Teucher <andy.teucher@gov.bc.ca>
2016-04-27 10:25:25 -0700

    add other layers and combine with carts

--- 02_clean.R
+++ 02_clean.R
@@ -16,7 +16,9 @@ library(sp)
 library(rgeos)
 library(raster)
 library(bcmaps) # install using devtools::install_github("bcgov/bcmaps")
-library(rmapshaper) # install using devtools::install_github("ateucher/rmapshaper")
+# library(rmapshaper) # install using devtools::install_github("ateucher/rmapshaper")
+
+source("fun.R")
 
 dir.create("tmp", showWarnings = FALSE)
 
@@ -26,11 +28,14 @@ carts <- readOGR("data/CARTS_Update_31122015.gdb", "CARTS_Update_31122015_Withou
 
 ## Read in the Conservation Lands database from: http://catalogue.data.gov.bc.ca/dataset/conservation-lands
 cl_gdb <- "data/BCGW_conservation_lands_fgdb/WCL_CONSERVATION_LANDS_SP.gdb"
-conservation_lands <- readOGR(cl_gdb, ogrListLayers(cl_gdb)[1])
+conservation_lands <- readOGR(cl_gdb, ogrListLayers(cl_gdb)[1], stringsAsFactors = FALSE)
 rm(cl_gdb)
 
+## Read in NGO conservation lands
+fee_simple_ngo_lands <- readOGR("data", "BC_NGO_ConsDB_FeeSimple_31Dec2014_updated02Nov2015", stringsAsFactors = FALSE)
+
 ## Extract just BC from CARTS
-bc_carts_orig <- carts[carts$LOC_E %in% c("British Columbia", "Offshore Pacific Marine"), ]
+bc_carts <- carts[carts$LOC_E %in% c("British Columbia", "Offshore Pacific Marine"), ]
 rm(carts)
 
 ## Subset only the Administrated Lands - Acquisitions and Leases
@@ -39,8 +44,12 @@ bc_admin_lands <- conservation_lands[conservation_lands$CONSERVATION_LAND_TYPE =
                                               conservation_lands$TENURE_TYPE == "Lease" |
                                               conservation_lands$TENURE_TYPE == "Transfer of Administration/Control"), ]
 
-## Transform CRS of bc_carts to BC Albers
-bc_carts <- spTransform(bc_carts_orig, CRS("+init=epsg:3005"))
+bc_admin_lands$PROTDATE <- max(bc_carts$PROTDATE, na.rm = TRUE)
+
+## Transform CRS of all layers to BC Albers
+bc_carts <- transform_albers(bc_carts)
+bc_admin_lands <- transform_albers(bc_admin_lands)
+fee_simple_ngo_lands <- transform_albers(fee_simple_ngo_lands)
 
 #### Simplify for testing
 ## devtools::install_github("ateucher/rmapshaper")
@@ -50,22 +59,27 @@ bc_carts <- spTransform(bc_carts_orig, CRS("+init=epsg:3005"))
 ####
 
 ## Check validity of polygons of bc_carts, fix with gBuffer trick, and check again:
-if (any(!gIsValid(bc_carts, byid = TRUE))) {
-  bc_carts <- gBuffer(bc_carts, byid = TRUE, width = 0)
-  any(!gIsValid(bc_carts, byid = TRUE))
-}
+bc_carts <- fix_geometry(bc_carts)
+bc_admin_lands <- fix_geometry(bc_admin_lands)
+fee_simple_ngo_lands <- fix_geometry(fee_simple_ngo_lands)
 
-# Get separate layers for terrestrial and marine protected areas
-bc_carts_t <- bc_carts[bc_carts$BIOME == "T", ]
-bc_carts_m <- bc_carts[bc_carts$BIOME == "M", ]
+bc_admin_lands_unioned <- single_sp_union(bc_admin_lands)
+fee_simple_ngo_lands_unioned <- single_sp_union(fee_simple_ngo_lands)
+bc_carts_unioned <- single_sp_union(bc_carts)
 
-## Union shapes to deal with overlaps (loses attributes but makes a dataframe of the shapes that combine to make each polygon)
-bc_carts_t_unioned <- raster::union(bc_carts_t) # This is incredibly slow.
-bc_carts_m_unioned <- raster::union(bc_carts_m)
+non_carts_unioned <- raster::union(bc_admin_lands_unioned, fee_simple_ngo_lands_unioned)
 
-bc_prot_areas <- raster::union(bc_carts_t_unioned, bc_admin_lands)
+all_unioned <- raster::union(bc_carts_unioned, non_carts_unioned)
 
-save(list = ls(), file = "tmp/bc_carts_clean.rda")
+# # Get separate layers for terrestrial and marine protected areas
+# bc_carts_t <- bc_carts[bc_carts$BIOME == "T", ]
+# bc_carts_m <- bc_carts[bc_carts$BIOME == "M", ]
+#
+# ## Union shapes to deal with overlaps (loses attributes but makes a dataframe of the shapes that combine to make each polygon)
+# bc_carts_t_unioned <- raster::union(bc_carts_t) # This is incredibly slow.
+# bc_carts_m_unioned <- raster::union(bc_carts_m)
+
+save(list = ls(), file = "tmp/prot_areas_clean.rda")
 rm(list = ls())
 
 
@@ -90,6 +104,9 @@ ecoregions_t <- rbind(ecoregions[!ecoregions$CRGNCD %in% c("GPB", m_ecoregions),
 ## Add marine portion of GPB back to marine ecoregions
 ecoregions_m <- rbind(ecoregions[ecoregions$CRGNCD %in% m_ecoregions, ],
                       gpb_marine[, setdiff(names(gpb_terrestrial), "rmapshaperid")])
+
+ecoregions_t$area <- gArea(ecoregions_t, byid = TRUE)
+ecoregions_m$area <- gArea(ecoregions_m, byid = TRUE)
 
 ## Create simplified versions for visualization
 ecoregions_t_simp <- ms_simplify(ecoregions_t, 0.01)
